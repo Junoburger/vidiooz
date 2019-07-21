5912a29082b00f1f7743c4fde56f790ccd99fe11
JunoJulius <juno.burger@officebooking.net>
2019-07-21 20:06:55 +0200

    added iframe plus description with onclick method

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import './VideoItem.css';
+import React from "react";
 
-const VideoItem = ({ video }) => {
-    return (
-    <div key={video.etag}>
-    <img src={video.snippet.thumbnails.medium.url}
-         alt={video.snippet.title} />
-        {video.snippet.title}
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div onClick={()=> onVideoSelect(video)} className="video-item item" key={video.etag}>
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
-    );
+  );
 };
-
-
 
 export default VideoItem;
